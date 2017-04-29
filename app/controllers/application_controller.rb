bae78873aa2c1902f058cef191b78e7c932884cf
Thomas Huisking <tomasfaustin@gmail.com>
2017-04-29 16:05:11 -0700

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Is this going to fucking work or what?"
+  end
+
+
+  
 end
