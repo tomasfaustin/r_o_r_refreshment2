93dedca3587359da1944cc53dc1940f9346acd01
Thomas Huisking <tomasfaustin@gmail.com>
2017-04-30 19:03:28 -0700

    Write test for root view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "Is this going to fucking work or what?"
-  end
-
-
   
+
+
+
 end
